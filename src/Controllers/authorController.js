97128c9e08bcba60fd1ade590ed9f0961fa9bc9d
HYDER ALI <21HYDERALI@GMAIL.COM>
2022-06-21 15:06:59 +0530

    project1

---
+++ src/Controllers/authorController.js
@@ -0,0 +1,17 @@
+const authorModel=require("../Models/authorModel")
+const createAuthor = async function (req, res) {
+    try {
+        let data = req.body
+        console.log(data)
+        if ( Object.keys(data).length != 0) {
+            let savedData= await authorModel.create(data)
+            res.status(201).send({ msg: savedData })
+        }
+        else res.status(400).send({ msg: "BAD REQUEST"})
+    }
+    catch (err) {
+        console.log("This is the error :", err.message)
+        res.status(500).send({ msg: "Error", error: err.message })
+    }
+}
+module.exports.createAuthor=createAuthor
