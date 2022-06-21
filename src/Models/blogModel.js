1e38ee2943219679e28cf36e040b3c8cf3d3979a
HYDER ALI <21HYDERALI@GMAIL.COM>
2022-06-21 18:32:54 +0530

    Schma and doc creation

---
+++ src/Models/blogModel.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const blogSchema = new mongoose.Schema({
+
+    title:{
+        type: String,
+        required: true
+    },
+
+    tags:[{
+       type: String
+    }],
+    
+    body:{
+        type: String,
+        required: true
+    },
+    authorId:{
+        type: ObjectId,
+        ref: "projectAuthor"
+    },
+    category:{
+        type: String,
+       // enum: ["technology", "entertainment", "life style", "food", "fashion"],
+        required: true
+    },
+    subCategory:{
+        type: [{
+            type: String,
+        }],
+        required:true
+    },
+    isPublished:{
+        type: Boolean,
+        default: true
+    },
+    isDeleted:{
+        type:Boolean,
+        default:false
+    }
+}, { timestamps: true });
+
+
+module.exports = mongoose.model('blogDoc',  blogSchema)
