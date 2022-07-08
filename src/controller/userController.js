092b1a363f4f3a7508da8f60bd1dedae9e7f268b
GautamMehta19 <mehtag810@gmail.com>
2022-07-09 01:00:35 +0530

    add

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -7,7 +7,7 @@ function isNum(val) {
 }
 const isValidString = function (value) {
     if (typeof value === "undefined" || value === null) return false
-    if (typeof value !== "string" || value.trim().length === 0) return false //"   "
+    if (typeof value !== "string" || value.trim().length === 0) return false
     return true;
 }
 
