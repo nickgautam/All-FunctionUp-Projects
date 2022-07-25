6d6ebd31ad6440741ce331cdca26584d643e7879
unknown <rajatkhobragade00@gmail.com>
2022-07-25 14:34:40 +0530

    nerw

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,22 +1,22 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-        fname: { type: String, required: true },
-        lname: { type: String, required: true },
-        email: { type: String, required: true, unique:true },
-        profileImage: { type: String, required: true},
-        phone:{type:String,required:true,unique:true},
-        password: { type: String, required: true },
+        fname: { type: String, required: true ,trim:true},
+        lname: { type: String, required: true ,trim:true},
+        email: { type: String, required: true, unique:true,trim:true },
+        profileImage: { type: String, required: true,trim:true},
+        phone:{type:String,required:true,unique:true,trim:true},
+        password: { type: String, required: true ,trim:true},
         address : { 
            shipping:{
-            street:{type:String,required:true},
-            city:{type:String,required:true},
-            pincode:{type:Number,required:true}
+            street:{type:String,required:true,trim:true},
+            city:{type:String,required:true,trim:true},
+            pincode:{type:Number,required:true,trim:true}
            },
            billing:{
-            street:{type:String,required:true},
-            city:{type:String,required:true},
-            pincode:{type:Number,required:true}
+            street:{type:String,required:true,trim:true},
+            city:{type:String,required:true,trim:true},
+            pincode:{type:Number,required:true,trim:true}
            }
         }
 
