fce78f6ba955817df426e87722dccbd15de4bd56
nickgautam <er.nishantgautam0011@gmail.com>
2022-07-27 16:22:23 +0530

    create productApi

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,4 +1,11 @@
 const productModel = require('../models/productModel')
+const {isValid,parseJSONSafely}= require("../validator/validator")
+const {uploadFile}= require("./awsController")
+
+const validTitle = /^[a-zA-Z ]{3,20}$/
+// const validEmail = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}/
+// const validPhoneNumber = /^[0]?[6789]\d{9}$/
+// const validPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*#?&]{8,15}$/;
 
 
 exports.createProducts =async (req,res)=>{
@@ -17,19 +24,23 @@ if (!price) return res.status(400).send({ status: false, message: "price is requ
 if (!currencyId) return res.status(400).send({ status: false, message: "currencyId is required" })
 if (!currencyFormat) return res.status(400).send({ status: false, message: "currencyFormat is required" })
 
-if (!isValid(title)) return res.status(400).send({ status: false, message: " title is invalid " })
+//if (!isValid(title)) return res.status(400).send({ status: false, message: " title is invalid " })
 if (!isValid(description)) return res.status(400).send({ status: false, message: " description  is invalid " })
 if (!isValid(price)) return res.status(400).send({ status: false, message: " price  is invalid " })
 if (!isValid(currencyId)) return res.status(400).send({ status: false, message: " currencyId is invalid " })
 if (!isValid(currencyFormat)) return res.status(400).send({ status: false, message: " currencyFormat  is invalid " })
-if (!isValid(style)) return res.status(400).send({ status: false, message: " style is invalid " })
 
-if(!NaN(description))return res.status(400).send({status:false, message:"description can't be a number"})
-if(NaN(installments))return res.status(400).send({status:false, message:"installments should be a number"})
+if(!validTitle.test(title)) return res.status(400).send({ status: false, message: " title is invalid " })
+if (typeof style === "string" && style.trim().length === 0) return res.status(400).send({ status: false, message: " style is invalid " })
+
+if(!isNaN(description))return res.status(400).send({status:false, message:"description can't be a number"})
+if(installments){
+    if(isNaN(installments))return res.status(400).send({status:false, message:"installments should be a number"})
+}
 if (!files.length) return res.status(400).send({ status: false, message: "Please Provide the Image file" })
 mimetype = files[0].mimetype.split("/") //---["image",""]
 if (mimetype[0] !== "image") return res.status(400).send({ status: false, message: "Please Upload the Image File only" })
-if (files && files.length > 0) var uploadedFileURL = await awsController.uploadFile(files[0])
+if (files && files.length > 0) var uploadedFileURL = await uploadFile(files[0])
 data.productImage = uploadedFileURL
 
 
@@ -39,7 +50,7 @@ if (["S","XS","M","X", "L","XXL", "XL"].indexOf(availableSizes) == -1) {
         message: " Enter a valid availableSizes S, XS, M, X, L, XXL, XL "
     })
 }
-data["deletedAt"] =new Date()
+
 let checkTitle= await productModel.findOne({title:title})
 if(checkTitle) return res.status(400).send({status:false, message:"title already exists"})
 
